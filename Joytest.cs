007eb376a594ded296d79699c7844b7958b1129b
limyt0 <80087763+limyt0@users.noreply.github.com>
2023-05-11 11:31:36 +0900

    Create Joytest.cs

---
+++ Joytest.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Joytest : MonoBehaviour
+{
+  void Start()
+  {
+      if (Input.GetKey(KeyCode.JoystickButton0))
+                {
+                    //A
+                }
+                if (Input.GetKey(KeyCode.JoystickButton1))
+                {
+                    //b
+                }
+                if (Input.GetKey(KeyCode.JoystickButton2))
+                {
+                    //x
+                }
+                if (Input.GetKey(KeyCode.JoystickButton3))
+                {
+                    //y
+                }
+                if (Input.GetKey(KeyCode.JoystickButton4))
+                {
+                    //LB
+                }if (Input.GetKey(KeyCode.JoystickButton5))
+                {
+                     //RB오른쪽 앞
+                }if (Input.GetKey(KeyCode.JoystickButton6))
+                {
+                    //ㅁㅁ
+                }if (Input.GetKey(KeyCode.JoystickButton7))
+                {
+                    //=
+                }if (Input.GetKey(KeyCode.JoystickButton8))
+                {
+                    //lefy knob
+                }if (Input.GetKey(KeyCode.JoystickButton9))
+                {
+                    //right knob
+                }
+  }
+}
